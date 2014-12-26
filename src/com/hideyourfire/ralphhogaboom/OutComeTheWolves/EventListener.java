526cfebf0e05d485be4eac49cf4a60e884aadbfc
ralphhogaboom <ralph@hogaboom.org>
2014-12-26 10:34:02 -0800

    Building sections for using config file, including new methods for getting / setting keys from Main.

--- src/com/hideyourfire/ralphhogaboom/OutComeTheWolves/EventListener.java
+++ src/com/hideyourfire/ralphhogaboom/OutComeTheWolves/EventListener.java
@@ -1,22 +1,74 @@
 package com.hideyourfire.ralphhogaboom.OutComeTheWolves;
 
+import java.util.Random;
+
+import org.bukkit.Location;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Sheep;
 import org.bukkit.entity.Wolf;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.EntitySpawnEvent;
+import org.bukkit.event.entity.EntityTargetEvent;
+import com.hideyourfire.ralphhogaboom.OutComeTheWolves.*;
 
 public class EventListener implements Listener {
 	
+	public static int randInt(int min, int max) {
+		Random rand = new Random();
+		int randomNum = rand.nextInt((max - min) + 1) + min;
+		return randomNum;
+	}
+
+	@EventHandler
+	public void onMobTarget(EntityTargetEvent event) {
+		Entity entity = event.getEntity();
+		Entity target = event.getTarget();
+		if (target instanceof Player) {
+			Player player = (Player)target;
+			if (player.getName().equalsIgnoreCase("mrcreeper") || player.getName().equalsIgnoreCase("cyclone56724")) {
+				event.setCancelled(true);
+			}
+		}
+	}
+	
 	@EventHandler
 	public void onWolfSpawn(CreatureSpawnEvent event) {
-		if (event.isCancelled()) {
-			return;
-		}
+		Location loc = event.getLocation();
 		Entity entity = event.getEntity();
-		if (entity instanceof Wolf) {
-			Wolf wolf = (Wolf)event.getEntity();
-			wolf.setAngry(true);
+		int random = randInt(0, 99);
+		
+		if (entity.getType() == EntityType.WOLF) {
+			if (Main.getPlugin().getConfig().getString("notifications").equalsIgnoreCase("yes")) {
+				Main.getPlugin().getLogger().info(entity.getType() + " spawned near " + loc.getBlockX() + " " + loc.getBlockY() + " " + loc.getBlockZ());
+			}
+			if (random < Main.getKey("aggressiveWolvesChance")) {
+				Wolf wolf = (Wolf)entity;
+				wolf.setAngry(true);
+				// get nearby entities
+				for (Entity nearbyEntity : wolf.getNearbyEntities(loc.getX(), loc.getY(), loc.getZ())) {
+					if (nearbyEntity instanceof Player) {
+						Player target = (Player)nearbyEntity;
+						if (target.getName().equalsIgnoreCase("mrcreeper") || target.getName().equalsIgnoreCase("cyclone56724")) {
+							// Do nothing.
+							wolf.setOwner(target);
+						} else {
+							wolf.setTarget((LivingEntity) nearbyEntity);
+						}
+					}
+				}
+			}
+		} else {
+			if (random < Main.getPlugin().getKey("changeOtherMobsChance")) {
+				if (loc.getY() > 65) {
+					event.setCancelled(true);
+					entity = loc.getWorld().spawnEntity(loc, EntityType.WOLF);
+				}
+			}
 		}
 	}
 }
--- src/com/hideyourfire/ralphhogaboom/OutComeTheWolves/Main.java
+++ src/com/hideyourfire/ralphhogaboom/OutComeTheWolves/Main.java
@@ -7,10 +7,18 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 	private static Plugin plugin;
-	
+	private int aggressiveWolvesChance;
+	private int changeFromOtherMobsChance;
+	private String notifications;
+
 	public void onEnable() {	
 		plugin = this;
 		registerEvents(this, new EventListener());
+		this.saveDefaultConfig(); // For first run, save default config file.
+		this.getConfig();
+		aggressiveWolvesChance = this.getConfig().getInt("aggressiveWolvesChance");
+		changeFromOtherMobsChance = this.getConfig().getInt("changeFromOtherMobsChance");
+		notifications = this.getConfig().getString("notifications");
 	}
 	
 	private void registerEvents(org.bukkit.plugin.Plugin plugin, Listener...listeners) {
@@ -20,6 +28,9 @@ public class Main extends JavaPlugin {
 	}
 
 	public void onDisable() {
+		this.getConfig().set("aggressiveWolvesChance", aggressiveWolvesChance);
+		this.getConfig().set("changeFromOtherMobsChance", changeFromOtherMobsChance);
+		this.getConfig().set("notifications", notifications);
 		plugin = null; // Stops memory leaks.
 	}
 	
@@ -27,5 +38,39 @@ public class Main extends JavaPlugin {
 		return plugin;
 	}
 
+	public void setNotifications(String notify) {
+		if (notify.equalsIgnoreCase("yes")) {
+			notifications = "yes";
+		}
+		if (notify.equalsIgnoreCase("no")) {
+			notifications = "no";
+		}
+	}
+	
+	public boolean getNotifications(String notify) {
+		if (notifications.equalsIgnoreCase("yes")) {
+			return true;
+		}
+		return false;
+	}
+
+	public int getKey(String key) {
+		if (key.equalsIgnoreCase("aggressiveWolvesChance")) {
+			return aggressiveWolvesChance;
+		}
+		if (key.equalsIgnoreCase("changeFromOtherMobsChance")) {
+			return changeFromOtherMobsChance;
+		}
+		return 0;
+	}
+	
+	public void setKey(String key, int value) {
+		if (key.equalsIgnoreCase("aggressiveWolvesChance")) {
+			aggressiveWolvesChance = value;
+		}
+		if (key.equalsIgnoreCase("changeFromOtherMobsChance")) {
+			changeFromOtherMobsChance = value;
+		}
+	}
 	
 }
