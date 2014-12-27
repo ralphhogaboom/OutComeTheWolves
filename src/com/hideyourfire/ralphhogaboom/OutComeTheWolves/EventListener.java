945cb68a138c9af0cfb37fec754971821a6161c4
ralphhogaboom <ralph@hogaboom.org>
2014-12-27 09:06:56 -0800

    Version 0.1, ready for public consumption.

--- src/com/hideyourfire/ralphhogaboom/OutComeTheWolves/EventListener.java
+++ src/com/hideyourfire/ralphhogaboom/OutComeTheWolves/EventListener.java
@@ -5,20 +5,21 @@ import java.util.Random;
 import org.bukkit.Location;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
-import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
-import org.bukkit.entity.Sheep;
 import org.bukkit.entity.Wolf;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
-import org.bukkit.event.entity.EntitySpawnEvent;
 import org.bukkit.event.entity.EntityTargetEvent;
 import com.hideyourfire.ralphhogaboom.OutComeTheWolves.Main;
 
 public class EventListener implements Listener {
 	
 	Main thisInstance;
+
+	public EventListener(Main instance) {
+		  thisInstance = instance;
+		}
 	
 	public static int randInt(int min, int max) {
 		Random rand = new Random();
@@ -28,11 +29,10 @@ public class EventListener implements Listener {
 
 	@EventHandler
 	public void onMobTarget(EntityTargetEvent event) {
-		Entity entity = event.getEntity();
 		Entity target = event.getTarget();
 		if (target instanceof Player) {
 			Player player = (Player)target;
-			if (player.getName().equalsIgnoreCase("mrcreeper") || player.getName().equalsIgnoreCase("cyclone56724")) {
+			if (player.hasPermission("outcomethewolves.bypass")) {
 				event.setCancelled(true);
 			}
 		}
@@ -43,31 +43,22 @@ public class EventListener implements Listener {
 		Location loc = event.getLocation();
 		Entity entity = event.getEntity();
 		int random = randInt(0, 99);
-		
 		if (entity.getType() == EntityType.WOLF) {
-			if (Main.getPlugin().getConfig().getString("notifications").equalsIgnoreCase("yes")) {
-				Main.getPlugin().getLogger().info(entity.getType() + " spawned near " + loc.getBlockX() + " " + loc.getBlockY() + " " + loc.getBlockZ());
-			}
 			if (random < thisInstance.getKey("aggressiveWolvesChance")) {
-				Main.getPlugin().getLogger().info("aggressiveWolvesChance = " + thisInstance.getKey("aggressiveWolvesChance"));
+				Main.getPlugin().getLogger().info("Spawning an angry wolf near " + loc.getX() + " " + loc.getY() + " " + loc.getZ() + ".");
 				Wolf wolf = (Wolf)entity;
 				wolf.setAngry(true);
-				// get nearby entities
 				for (Entity nearbyEntity : wolf.getNearbyEntities(loc.getX(), loc.getY(), loc.getZ())) {
 					if (nearbyEntity instanceof Player) {
 						Player target = (Player)nearbyEntity;
-						if (target.getName().equalsIgnoreCase("mrcreeper") || target.getName().equalsIgnoreCase("cyclone56724")) {
-							// Do nothing.
+						if (target.hasPermission("outcomethewolves.own")) {
 							wolf.setOwner(target);
-						} else {
-							wolf.setTarget((LivingEntity) nearbyEntity);
 						}
 					}
 				}
 			}
 		} else {
 			if (random < thisInstance.getKey("changeOtherMobsChance")) {
-				Main.getPlugin().getLogger().info("changeOtherMobsChance = " + thisInstance.getKey("changeOtherMobsChance"));
 				if (loc.getY() > 65) {
 					event.setCancelled(true);
 					entity = loc.getWorld().spawnEntity(loc, EntityType.WOLF);
--- src/com/hideyourfire/ralphhogaboom/OutComeTheWolves/Main.java
+++ src/com/hideyourfire/ralphhogaboom/OutComeTheWolves/Main.java
@@ -8,17 +8,15 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Main extends JavaPlugin {
 	private static Plugin plugin;
 	private int aggressiveWolvesChance;
-	private int changeFromOtherMobsChance;
-	private String notifications;
+	private int changeOtherMobsChance;
 
 	public void onEnable() {	
 		plugin = this;
-		registerEvents(this, new EventListener());
+		registerEvents(this, new EventListener(this));
 		this.saveDefaultConfig(); // For first run, save default config file.
 		this.getConfig();
-		aggressiveWolvesChance = this.getConfig().getInt("aggressiveWolvesChance");
-		changeFromOtherMobsChance = this.getConfig().getInt("changeFromOtherMobsChance");
-		notifications = this.getConfig().getString("notifications");
+		aggressiveWolvesChance = this.getConfig().getInt("aggressiveWolves");
+		changeOtherMobsChance = this.getConfig().getInt("otherMobsIntoWolves");
 	}
 	
 	private void registerEvents(org.bukkit.plugin.Plugin plugin, Listener...listeners) {
@@ -28,38 +26,19 @@ public class Main extends JavaPlugin {
 	}
 
 	public void onDisable() {
-		this.getConfig().set("aggressiveWolvesChance", aggressiveWolvesChance);
-		this.getConfig().set("changeFromOtherMobsChance", changeFromOtherMobsChance);
-		this.getConfig().set("notifications", notifications);
 		plugin = null; // Stops memory leaks.
 	}
 	
 	public static Plugin getPlugin() {
 		return plugin;
 	}
-
-	public void setNotifications(String notify) {
-		if (notify.equalsIgnoreCase("yes")) {
-			notifications = "yes";
-		}
-		if (notify.equalsIgnoreCase("no")) {
-			notifications = "no";
-		}
-	}
 	
-	public boolean getNotifications(String notify) {
-		if (notifications.equalsIgnoreCase("yes")) {
-			return true;
-		}
-		return false;
-	}
-
 	public int getKey(String key) {
 		if (key.equalsIgnoreCase("aggressiveWolvesChance")) {
 			return aggressiveWolvesChance;
 		}
-		if (key.equalsIgnoreCase("changeFromOtherMobsChance")) {
-			return changeFromOtherMobsChance;
+		if (key.equalsIgnoreCase("changeOtherMobsChance")) {
+			return changeOtherMobsChance;
 		}
 		return 0;
 	}
@@ -68,9 +47,8 @@ public class Main extends JavaPlugin {
 		if (key.equalsIgnoreCase("aggressiveWolvesChance")) {
 			aggressiveWolvesChance = value;
 		}
-		if (key.equalsIgnoreCase("changeFromOtherMobsChance")) {
-			changeFromOtherMobsChance = value;
+		if (key.equalsIgnoreCase("changeOtherMobsChance")) {
+			changeOtherMobsChance = value;
 		}
 	}
-	
 }
